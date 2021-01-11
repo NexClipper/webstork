2aa709e71e6b0db562ba0840544ffc7ff188964c
peter <folimworld@gmail.com>
2021-01-11 16:05:26 +0900

    TEST RUN

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,13 @@ FROM nginx:1.19.6-alpine
 #alpine:3.12.3 - 2020.12.31
 LABEL maintainer="NexCloud Peter <peter@nexclipper.io>"
 
-RUN echo "NexClipper"
+#### Default Setting
 RUN apk add --no-cache --update curl
+COPY entrypoint.sh /entrypoint.sh
 
 ## KubeCTL Download ##
 RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
     chmod +x kubectl && mv kubectl /usr/bin/
 
 ## CMD
+CMD ["/bin/sh", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+KUBENAMESPACE="nexclipper"
+PORTFORWARDLOG="/tmp/webstork_portforward.log"
+
+#TEST
+# promlens
+kubectl port-forward --address 0.0.0.0 service/nc-promlens -n ${KUBENAMESPACE} 8080:80 \
+>> $PORTFORWARDLOG 2>&1 &
+kubectl port-forward --address 0.0.0.0 service/nc-promscale-connector -n ${KUBENAMESPACE} 9201:9201 \
+>> $PORTFORWARDLOG 2>&1 &
+
+# grafana
+kubectl port-forward --address 0.0.0.0 service/nc-grafana -n ${KUBENAMESPACE} 80:80 \
+>> $PORTFORWARDLOG 2>&1 &
+
+# prometheus
+kubectl port-forward --address 0.0.0.0 service/nc-prometheus-server -n ${KUBENAMESPACE} 9090:80 \
+>> $PORTFORWARDLOG 2>&1 &
+
+# alertmanager
+kubectl port-forward --address 0.0.0.0 service/nc-prometheus-alertmanager -n ${KUBENAMESPACE} 8081:80 \
+>> $PORTFORWARDLOG 2>&1 &
+
+# pushgateway
+kubectl port-forward --address 0.0.0.0 service/nc-prometheus-pushgateway -n ${KUBENAMESPACE} 9091:9091 \
+>> $PORTFORWARDLOG 2>&1 &
+
+
+tail -f $PORTFORWARDLOG
